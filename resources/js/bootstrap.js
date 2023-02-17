90755d99f7c47681392b83a5bd9f8d2ec038bf88
viktorasbuivydas <viktoras162@gmail.com>
2023-02-17 21:35:53 +0200

    .

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -26,12 +26,10 @@ window.Pusher = Pusher;
 window.Echo = new Echo({
     broadcaster: "pusher",
     key: import.meta.env.VITE_PUSHER_APP_KEY,
-    wsHost: import.meta.env.VITE_PUSHER_HOST
-        ? import.meta.env.VITE_PUSHER_HOST
-        : `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
+    wsHost: import.meta.env.VITE_PUSHER_HOST,
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-    wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
-    wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
-    forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? "https") === "https",
-    enabledTransports: ["ws", "wss"],
+    wsPort: 6001,
+    forceTLS: false,
+    disableStats: true,
+    scheme: import.meta.env.VITE_PUSHER_SCHEME,
 });
